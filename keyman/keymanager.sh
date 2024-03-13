de7c351f4dcc62bdc8579ccf5e97bf4a53547157
krishna2803 <kpandey81930@gmail.com>
2024-03-13 23:58:12 +0530

    Added ✨colours✨

--- keyman/keymanager.sh
+++ keyman/keymanager.sh
@@ -5,46 +5,52 @@ source manage_gpg_keys.sh
 source manage_ssh_keys.sh
 
 banner() {
+    echo -en $BIRed
     echo "  _  __          __  __                                   ";
     echo " | |/ /___ _   _|  \/  | __ _ _ __   __ _  __ _  ___ _ __ ";
     echo " | ' // _ \ | | | |\/| |/ _\` | '_ \ / _\` |/ _\` |/ _ \ '__|";
     echo " | . \  __/ |_| | |  | | (_| | | | | (_| | (_| |  __/ |   ";
     echo " |_|\_\___|\__, |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|   ";
     echo "           |___/                          |___/           ";
+    echo -en $Color_Off
 }
 
 menu() {
+    echo -en $BIWhite
     echo What do you want to do?
+    echo -en $Color_Off
     
-    echo "1 => create ssh key"
-    echo "2 => add ssh key to ssh agent"
-    echo "3 => create gpg key"
-    echo "4 => add gpg key to git and github"
-    echo "0 => exit"
+    echo -en $BIYellow
+    echo "1 => Create ssh key"
+    echo -en $BIGreen
+    echo "2 => Add ssh key to ssh agent"
+    echo -en $BIBlue
+    echo "3 => Create gpg key"
+    echo -en $BIPurple
+    echo "4 => Add gpg key to git and github"
+    echo -en $BIBlack
+    echo "0 => Exit"
+    echo -en $Color_Off
+    echo -en "> "
 
     read choice
     if [ $choice -eq 1 ]; then
+        echo -en $BIYellow
        ssh_create
     elif [ $choice -eq 2 ]; then 
+        echo -en $BIGreen
         echo -en "Write the path of the keyfile: "
         read keyfile
         ssh_add_key $keyfile
     elif [ $choice -eq 3 ]; then
+        echo -en $BIPurple
+        echo -en $BIBlue
         gpg_create
     elif [ $choice -eq 4 ]; then
-        echo -e "Please select a key from the list"
-        echo 
-        gpg --list-secret-keys --keyid-format=long
+        
+        echo -en $BIPurple
 
-
-        read gitkey
-
-        gpg --armour --export $gitkey
-
-        git config --global user.signingkey $gitkey
-        git config --global commit.gpgsign true
-
-        echo -e "Add this public key to your github account"
+        gpg_add_key_to_git
 
         menu
 
--- keyman/manage_gpg_keys.sh
+++ keyman/manage_gpg_keys.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/bash
 
+source colours.sh
+
 gpg_create() {
     gpg --full-generate-key
+}
+
+
+gpg_add_key_to_git() {
+    echo -en $BIWhite
+    echo -e "Please select a key from the list"
+    echo 
+    echo -en $Color_Off
+    gpg --list-secret-keys --keyid-format=long
+
+    read gitkey
+
+    gpg --armour --export $gitkey
+
+    git config --global user.signingkey $gitkey
+    git config --global commit.gpgsign true
+
+
+    echo -en $BIWhite
+    echo -e "Add this public key to your github account"
+    echo -en $Color_Off
 }
