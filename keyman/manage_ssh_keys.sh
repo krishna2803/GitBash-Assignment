7552b24c4d4045dea97c41c445216727ea54bcab
krishna2803 <kpandey81930@gmail.com>
2024-03-15 18:48:59 +0530

    Added the ability to choose an algorithm for ssh key generation and also the interactive input of email

--- keyman/manage_ssh_keys.sh
+++ keyman/manage_ssh_keys.sh
@@ -1,7 +1,56 @@
 #!/usr/bin/bash
 
+source colours.sh
+
 ssh_create() {
-    ssh-keygen
+    echo -e "Choose an algorithm for generating key: "
+    echo -en $BIWhite
+    echo "1 => ED-25519 (default)"
+    echo -en $BIBlack
+    echo "2 => RSA"
+    echo -en "> "
+
+    read algo
+
+    echo -en $BWhite
+    if [ -z $algo ] || [ $algo -eq 1 ] ; then
+
+        echo -en $BIWhite
+        echo -en "Enter your email: "
+        echo -en $BIGreen
+
+        read email
+
+        echo -en $BIWhite
+
+        ssh-keygen -t ed25519 -C $email
+
+    elif [ $algo -eq 2 ]; then
+        echo -e "Choose bit length: (default: 4096)"
+        read bit_length
+
+        if [ -z $bit_length ]; then
+            bit_length=4096
+        fi
+
+
+        echo -en $BIWhite
+        echo -en "Enter your email: "
+        echo -en $BIGreen
+
+        read email
+
+        echo -en $BIWhite
+
+        ssh-keygen -t rsa -b $bit_length -C $email
+    else
+        echo -en $BIRed
+        echo -e
+        echo -e "Invalid choice! Returning back to menu." 
+        echo -e
+        sleep 0.5
+        return
+    fi
 }
 
 ssh_add_key() {
